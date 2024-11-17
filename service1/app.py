ff4534f0d9962106f28dc3847fb0ca163a6f6ea4
Hissatsu265 <lory265265@gmail.com>
2024-11-17 08:59:01 +0700

    port

--- service1/app.py
+++ service1/app.py
@@ -30,4 +30,4 @@ def analyze_sentiment():
     return jsonify(response), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port=7991)
+    app.run(debug=True, host='0.0.0.0',port=5000)
--- service2/app.py
+++ service2/app.py
@@ -35,4 +35,4 @@ def translate_text():
     return jsonify(response), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0',port=5001)
